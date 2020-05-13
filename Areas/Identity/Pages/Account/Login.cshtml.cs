656a8262fdd124a6f3aceed7be7c1fb05d5fb38b
cepardoe <cepardoe@unal.edu.co>
2020-05-13 00:26:07 -0500

    Se configura el bootstrap. Se agrega el login. Se crea la pagina de login. Se hace diseño de pagina de login. Queda pendiente bootstrap a las paginas de usuarios, zonas y terminales

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace CronosegAccess.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "¿Recordarme?")]
             public bool RememberMe { get; set; }
         }
 
